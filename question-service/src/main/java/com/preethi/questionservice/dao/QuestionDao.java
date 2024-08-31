aa39b9a30d843d7b79e7601e34b4c24137056ad7
preethi024 <89218062+preethi024@users.noreply.github.com>
2024-08-31 14:20:14 +0530

    Add files via upload

---
+++ question-service/src/main/java/com/preethi/questionservice/dao/QuestionDao.java
@@ -0,0 +1,19 @@
+package com.preethi.questionservice.dao;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.preethi.questionservice.model.Question;
+
+import java.util.List;
+
+@Repository
+public interface QuestionDao extends JpaRepository<Question, Integer> {
+
+    List<Question> findByCategory(String category);
+
+    @Query(value = "SELECT q.id FROM question q Where q.category=:category ORDER BY RAND() LIMIT :numQ", nativeQuery = true)
+    List<Integer> findRandomQuestionsByCategory(String category, int numQ);
+}
